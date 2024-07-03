78394d75950d1b2edf3bf191173c2ccba2549188
Alqorni30 <vinnbayo@gmail.com>
2024-07-03 17:09:22 +0700

    first commit'

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const app = express();
+
+dotenv.config();
+
+const URL = process.env.URL_LOCAL;
+// const URL = process.env.URL_PRODUCTION;
+const PORT = process.env.PORT;
+
+app.use(cors({ origin: URL }));
+
+app.use(express.json());
+
+app.get("/api", (req, res) => {
+  res.send("Welcome to my API Bayo");
+});
+
+const productController = require("./product/product.controller");
+
+app.use("/products", productController);
+
+app.listen(PORT, () => {
+  console.log("Express API running in port: " + PORT);
+});
